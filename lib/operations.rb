e3a8e6bd4558206f27f34af7fa37ca13ba1dcdbb
imtripleq <flamework89@hotmail.com>
2022-01-13 10:05:32 +1100

    done

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,14 @@
 def unsafe?(speed)
-speed < 40 || speed > 60 ? true : false
+ if speed < 40 || speed > 60 
+    return true
+ else false
+ end
 end
 
 
 
 def not_safe?(speed)
-	if speed < 40 || speed > 60 
-         true
-    end
+ speed < 40 || speed > 60 ? true : false
 end
 	
 
